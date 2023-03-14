7b918dde10205adbe0f333813ecaea3d5813c768
Your Name <tipvireakrith.kim@student.cadt.edu.kh>
2023-03-14 10:53:32 +0700

    Commit and Push

--- src/main/java/com/example/finalproject/Controller.java
+++ src/main/java/com/example/finalproject/Controller.java
@@ -4,20 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Controller extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Controller.class.getResource("MainPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("Main Page ");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+
+        launch(args);
     }
 }
---
+++ src/main/java/com/example/finalproject/MainPage.java
@@ -0,0 +1,24 @@
+package com.example.finalproject;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+
+public class MainPage {
+
+    private String signup;
+    private String login;
+
+    public void SignUp(ActionEvent event){
+        System.out.println("Sign Up");
+    }
+
+    public void Login(ActionEvent event){
+        System.out.println("Login");
+
+    }
+
+
+}
+
+
+
